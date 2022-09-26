67330dc25a1e4ef6ca803d59c9bb13b9ea1449db
JakeHui <112128322+JakeHui@users.noreply.github.com>
2022-09-26 09:48:43 -0700

    Update billcode.c

--- billcode.c
+++ billcode.c
@@ -8,47 +8,60 @@
 #include <stdio.h>
 #include <math.h>
 
+double solve (double tax, double tip, double bill);
+
 int main(void)
 {
-    int nofpeople;
-    int tax;
-    int tip;
-    int bill;
-    int totalpay;
-    int personpay;
+    double nofpeople;
+    double tax;
+    double tip;
+    double bill;
+    double personpay;
+    double total;
 
 // finding the number of people
     do
     {
-        nofpeople = get_int("Number of people: ");
+        nofpeople = get_double("Number of people: ");
     }
     while (nofpeople < 1);
 
 // finding the tax amount
     do
     {
-        tax = get_int("Tax Precentage: ");
+        tax = get_double("Tax Precentage: ");
     }
     while (tax < 0);
 
 // finding the tip precentage
     do
     {
-        tip = get_int("Tip precentage of bill: ");
+        tip = get_double("Tip precentage of bill: ");
     }
     while (tip < 0);
 
 // finding the amount of the bill
     do
     {
-        bill = get_int("How much was the bill: ");
+        bill = get_double("How much was the bill: ");
     }
     while (bill < 0);
 
-    // calculate how much each person pays
-    totalpay = (bill)+(tip*bill/100)+(tax*bill/100);
-    //dividing by 100 makes it a precentage
-    personpay = (totalpay/nofpeople);
-    printf("Each person pays: %i", personpay);
+    total = solve(bill, tip, tax);
+    personpay = (total/nofpeople);
+
+    // printing
+    printf("Each person pays: %f", personpay);
     printf("\n");
+
+    return 0;
 }
+
+double solve (double tax, double tip, double bill) {
+    double total = 0;
+    total = (bill)+(tip*bill/100)+(tax*bill/100);
+    // calculating total cost
+    //dividing by 100 makes it a precentage
+    return total;
+}
+
