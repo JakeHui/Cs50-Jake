448509491a0e7c0bf2695ff6822a0f84f879fd88
JakeHui <112128322+JakeHui@users.noreply.github.com>
2023-01-26 20:39:30 -0800

    Update day34.c

--- day34.c
+++ day34.c
@@ -4,16 +4,13 @@
 
 int main()
 {
-
     int* ptr;
     int n, i;
 
     printf("Enter number of elements:"); // get no of elements
     scanf("%d",&n);
     printf("Entered number of elements: %d\n", n);
-
-    //allocate memory using malloc()
-    ptr = (int*)malloc(n * sizeof(int));
+    ptr = (int*)malloc(n * sizeof(int)); //llocate memory using malloc()
 
     if (ptr == NULL) 
     {
@@ -21,20 +18,17 @@ int main()
         exit(0); // Exit program if memory allocation failed
     }
     else {
-
         // Memory has been successfully allocated
         printf("Memory successfully allocated using malloc.\n");
 
         for (i = 0; i < n; ++i) {
             ptr[i] = i + 1;
         }
-
         printf("The elements of the array are: ");
         for (i = 0; i < n; ++i) {
             printf("%d, ", ptr[i]);
         }
         printf("\n\n");
-
     }
 
     int* ptrr;
@@ -42,7 +36,6 @@ int main()
 
     int j = get_int("Enter number of elements: ");
     printf("Entered number of elements: %d\n", j); // Printing j for user to confirm
-
     // Calloc to initialize the desired number of elements in the size of int
     ptrr = (int*)calloc(j, sizeof(int));
 
@@ -52,7 +45,7 @@ int main()
         printf("Memory not allocated.\n");
         exit(0); // Exit program if memory allocation failed
     }
-    else // If not null, then memory has been successfully allocated
+        else // If not null, then memory has been successfully allocated
     {
         printf("Memory successfully allocated using calloc.\n");
         for (k = 0; k < j; ++k) // To put values into the memory with array, pointer and for loop. k<j ensures stops before last element is reached
@@ -62,8 +55,7 @@ int main()
 
         // Print the elements of the array
         printf("The elements of the array are: ");
-        for (k = 0; k < j; ++k)
-        {
+        for (k = 0; k < j; ++k) {
             printf("%d, ", ptrr[k]);
         }
 
@@ -75,8 +67,6 @@ int main()
     int z = get_int("Pick a new size for ptrr: ");
 
     ptrr = realloc(ptrr, z * sizeof(int)); //re-allocate ptrr with realloc() to size of new input
-
-
     if (ptrr == NULL) // If failed to allocate, pointer would be null
     {
         printf("Memory not allocated.\n");
@@ -92,14 +82,10 @@ int main()
         }
 
         printf("The elements of the array are: ");   //Print
-        for (int m = 0; m < z; ++m)
-        {
+        for (int m = 0; m < z; ++m) 
             printf("%d, ", ptrr[m]);
         }
-
         printf("\n\n");
     }
-
-
     return 0; // Free the memory
-}
+  }
