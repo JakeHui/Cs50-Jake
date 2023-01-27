b245cfaf0d3d3c8f7015d776ab3cd88949eba21f
JakeHui <112128322+JakeHui@users.noreply.github.com>
2023-01-26 20:35:04 -0800

    Update day34.c

--- day34.c
+++ day34.c
@@ -1,74 +1,105 @@
-//malloc
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.h>
 
 int main()
 {
 
-    // This pointer will hold the
-    // base address of the block created
     int* ptr;
     int n, i;
 
-    // Get the number of elements for the array
-    printf("Enter number of elements:");
+    printf("Enter number of elements:"); // get no of elements
     scanf("%d",&n);
     printf("Entered number of elements: %d\n", n);
 
-    // Dynamically allocate memory using malloc()
+    //allocate memory using malloc()
     ptr = (int*)malloc(n * sizeof(int));
 
-    // Check if the memory has been successfully
-    // allocated by malloc or not
-    if (ptr == NULL) {
+    if (ptr == NULL) 
+    {
         printf("Memory not allocated.\n");
-        exit(0);
+        exit(0); // Exit program if memory allocation failed
     }
     else {
 
         // Memory has been successfully allocated
         printf("Memory successfully allocated using malloc.\n");
 
-        // Get the elements of the array
         for (i = 0; i < n; ++i) {
             ptr[i] = i + 1;
         }
 
-        // Print the elements of the array
         printf("The elements of the array are: ");
         for (i = 0; i < n; ++i) {
             printf("%d, ", ptr[i]);
         }
-        printf("\n");
-    }
-    free(ptr);
+        printf("\n\n");
 
-    int * ptr2;
-    ptr2 = (int*)calloc(n, sizeof(int));
-    for (i = 0; i < n; ++i)
+    }
+
+    int* ptrr;
+    int k;
+
+    int j = get_int("Enter number of elements: ");
+    printf("Entered number of elements: %d\n", j); // Printing j for user to confirm
+
+    // Calloc to initialize the desired number of elements in the size of int
+    ptrr = (int*)calloc(j, sizeof(int));
+
+    // Check if the memory has been successfully allocated by memory because of multiple factors
+    if (ptrr == NULL)
     {
-        ptr2[i] = i + 1;
+        printf("Memory not allocated.\n");
+        exit(0); // Exit program if memory allocation failed
     }
-
-    // Print the elements of the array
-    printf("The elements of the second array are: ");
-    for (i = 0; i < n; ++i)
+    else // If not null, then memory has been successfully allocated
     {
-        printf("%d, ", ptr2[i]);
+        printf("Memory successfully allocated using calloc.\n");
+        for (k = 0; k < j; ++k) // To put values into the memory with array, pointer and for loop. k<j ensures stops before last element is reached
+        {
+            ptrr[k] = k + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (k = 0; k < j; ++k)
+        {
+            printf("%d, ", ptrr[k]);
+        }
+
+        printf("\n\n");
     }
-    printf("\n");
+
+    free(ptr); // Freeing memory
+
+    int z = get_int("Pick a new size for ptrr: ");
+
+    ptrr = realloc(ptrr, z * sizeof(int)); //re-allocate ptrr with realloc() to size of new input
 
 
-    ptr2 = realloc(ptr2, (n+90) *sizeof(int));
-    return 0;
+    if (ptrr == NULL) // If failed to allocate, pointer would be null
+    {
+        printf("Memory not allocated.\n");
+        exit(0); // Exit program if memory allocation failed
+    }
+    else // If not null, then memory has been successfully allocated
+    {
+        printf("Memory successfully re-allocated using realloc.\n");
+
+        for (int m = 0; m < z; ++m) // To put values into the memory with array, pointer and for loop. k<j ensures stops before last element is reached
+        {
+            ptrr[m] = m + 1;
+        }
+
+        printf("The elements of the array are: ");   //Print
+        for (int m = 0; m < z; ++m)
+        {
+            printf("%d, ", ptrr[m]);
+        }
+
+        printf("\n\n");
+    }
+
+
+    return 0; // Free the memory
 }
-
-//then use calloc()
-
-//ptr = (int*)calloc(n, sizeof(int));
-
-// free
-
- // Dynamically re-allocate memory using realloc()
- //ptr = realloc(ptr, n * sizeof(int));
